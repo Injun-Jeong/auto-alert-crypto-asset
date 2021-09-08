2b5c5ace9428c4f8b43bc248d92f67cf39e5d485
injun <dhak45@gmail.com>
2021-09-08 06:05:41 +0000

    feat: shrimp keeper v1.0.1

--- server/api/upbit/upbit.ctrl.js
+++ server/api/upbit/upbit.ctrl.js
@@ -1,6 +1,9 @@
 const request = require('sync-request');
+const key = require('../../../env/key.json');
+const ticker = key["market ticker"];
 const url = 'https://api.upbit.com/v1/ticker?markets=';
 const options = {method: 'GET', headers: {Accept: 'application/json'}};
+
 
 /** 시세 조회
  * getPrice
@@ -9,9 +12,16 @@ const options = {method: 'GET', headers: {Accept: 'application/json'}};
  */
 const getPrice = function(req, res) {
     console.info("시세 조회 메서드 호출");
-    const ticker = req.params.ticker;
+    const reqTicker = req.query.ticker;
+    let urlInfo = url;
 
-    const response = request('GET', url.concat("KRW-").concat(ticker), options);
+    if ( reqTicker == null ) {
+        urlInfo = urlInfo.concat(ticker);
+    } else {
+        urlInfo = urlInfo.concat(reqTicker);
+    }
+
+    const response = request('GET', urlInfo, options);
 
     /* 조회 결과값 */
     console.log('GET /upbit/%s 응답 메시지', ticker);
